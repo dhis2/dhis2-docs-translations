fe6c235a12d86b5ff86e9a0aba900c017d2b2c51
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2019-03-01 15:47:56 +0100

    refactor: parameterise languages

--- backup.sh
+++ backup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rm -rf fr
-tx pull -l fr
-git add fr
+# include translated languages here
+# e.g. to include French and Spanish:
+# LANGUAGES=( fr es )
+LANGUAGES=( fr )
+
+for lang in ${LANGUAGES[@]}
+do
+  rm -rf ${lang}
+  tx pull -l ${lang}
+  git add ${lang}
+done
 git commit -m "sync latest from transifex"
 git push origin master
