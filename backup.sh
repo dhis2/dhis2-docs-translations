a73f7dbc9e65a72f4402d047346f98918c20661c
Philip-Larsen-Donnelly <phil@dhis2.org>
2019-05-23 00:22:05 +0200

    fixes

--- backup.sh
+++ backup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for branch in "2.29" "2.30" "2.31" "2.32" "master"
+BRANCHES=( "2.29" "2.30" "2.31" "2.32" "master" )
+
+for branch in ${BRANCHES[@]}
 do
   git checkout -B $branch
-  git pull
+  git pull origin $branch
+  rm -rf .tx
   tx config mapping-remote https://www.transifex.com/hisp-uio/dhis-2-documentation-${branch//.}
 
   # include translated languages here
@@ -16,8 +19,10 @@ do
     rm -rf ${lang}
     rm -f source_me
     tx pull -l ${lang}
-    git add ${lang}
+    git add translations
   done
+
+  rm -rf .tx
   git commit -m "sync latest doc translations for branch $branch"
   git push origin $branch
 
