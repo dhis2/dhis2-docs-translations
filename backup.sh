ccf76c76fe98cb706136f3f8879f541092857c54
Philip-Larsen-Donnelly <phil@dhis2.org>
2019-05-23 00:15:50 +0200

    generate txconfig from transifex content

--- backup.sh
+++ backup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# include translated languages here
-# e.g. to include French and Spanish:
-# LANGUAGES=( fr es )
-LANGUAGES=( fr )
-
-for lang in ${LANGUAGES[@]}
+for branch in "2.29" "2.30" "2.31" "2.32" "master"
 do
-  rm -rf ${lang}
-  tx pull -l ${lang}
-  git add ${lang}
+  git checkout -B $branch
+  git pull
+  tx config mapping-remote https://www.transifex.com/hisp-uio/dhis-2-documentation-${branch//.}
+
+  # include translated languages here
+  # e.g. to include French and Spanish:
+  # LANGUAGES=( fr es )
+  LANGUAGES=( fr )
+
+  for lang in ${LANGUAGES[@]}
+  do
+    rm -rf ${lang}
+    rm -f source_me
+    tx pull -l ${lang}
+    git add ${lang}
+  done
+  git commit -m "sync latest doc translations for branch $branch"
+  git push origin $branch
+
 done
-git commit -m "sync latest from transifex"
-git push origin master
